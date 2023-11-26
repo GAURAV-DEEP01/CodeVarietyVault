53df3e8b4dbd1d930b6d9fa13da75575b90ddfde
Gaurav <gauravdeep8660@gmail.com>
2023-11-26 17:03:32 +0530

    init

---
+++ c/ptrToPtr.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int **ptp;
+    int num = 6;
+    int *p;
+    p=&num;
+    ptp= &p;
+    printf("num is \t%d\n",num);
+    printf("*p \t%ld\n",*p);
+    printf("just p\t%x\n",p);
+    printf("&p \t%x\n",&p);
+    printf("**ptp \t%d\n",**ptp);
+    printf("*ptp \t%x\n",*ptp);
+    printf("ptp \t%x\n",ptp);
+    printf("&ptp \t%x\n",&ptp);
+}
