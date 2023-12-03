7ae94805689f83b1f778c0f0f23912f55245365f
Gaurav <gauravdeep8660@gmail.com>
2023-12-03 18:59:31 +0530

    new

--- c/FCFS_Scheduling.c
+++ c/FCFS_Scheduling.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stdlib.h>   
 void main(){
     int n,at[20],bt[20],wt[20],tt[20],ft[20],st[20],totWt = 0,totTat = 0;
-    unsigned int idleTime;
+    int idleTime = 0;
     float avgWt,avgTat;
     printf("Enter the number of processes\n");
     scanf("%d",&n);
@@ -12,7 +12,22 @@ void main(){
         scanf("%d",&at[i]);
         printf("Burst Time of P%d : ",i+1);
         scanf("%d",&bt[i]);   
-    }    
+    } 
+    for (int i = 0; i < n; i++){
+        int min = i;
+        for(int j= i+1;j<n;j++){
+            if(at[min]>at[j]) min = j;
+        }
+        if(min!=i){
+            int temp = at[min];
+            at[min] = at[i];
+            at[i] = temp;
+            temp = bt[min];
+            bt[min] = bt[i];
+            bt[i] = temp;  
+        }
+    }
+       
     for (int i = 0; i <n; i++){
         if(i==0){
             st[i] = at[i];
@@ -22,7 +37,7 @@ void main(){
             if(ft[i-1]-at[i]<0){
                 st[i] = at[i];
                 wt[i] = 0;
-                idleTime =abs(ft[i-1]-at[i]);
+                idleTime +=abs(ft[i-1]-at[i]);
             } else{
                 st[i] = ft[i-1];
                 wt[i] =st[i] - at[i];
@@ -38,7 +53,7 @@ void main(){
     }
     avgTat = (float)totTat/n;
     avgWt = (float)totWt/n;
-    printf("Average wait time is : %d\n",avgWt);
-    printf("Average turn around time is : %d\n",avgTat);   
+    printf("Average wait time is : %.3f\n",avgWt);
+    printf("Average turn around time is : %.3f\n",avgTat);   
     printf("Total Idle time is : %d\n\n",idleTime);  
 }
